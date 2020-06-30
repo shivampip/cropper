1d4f8d81f217fb4cd046b778f6c7be647f3c0df5
Shivam Agrawal <shivam301296@gmail.com>
2020-06-30 05:59:45 +0000

    Updates

---
+++ precache-manifest.9842fcbefd26d7c31979749b18d58e94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8939ac1cfaff45574f1550761e72acd3",
+    "url": "/cropper/index.html"
+  },
+  {
+    "revision": "c5f7c94363f1648b6dd0",
+    "url": "/cropper/static/css/2.34dcc6c8.chunk.css"
+  },
+  {
+    "revision": "34842def74545eb661ff",
+    "url": "/cropper/static/css/main.9bb2a136.chunk.css"
+  },
+  {
+    "revision": "c5f7c94363f1648b6dd0",
+    "url": "/cropper/static/js/2.bbb4f302.chunk.js"
+  },
+  {
+    "revision": "9104c4de9f0dfb18428ab4aa6452ba95",
+    "url": "/cropper/static/js/2.bbb4f302.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34842def74545eb661ff",
+    "url": "/cropper/static/js/main.bb1f0e91.chunk.js"
+  },
+  {
+    "revision": "863d79bc6a6035d1e810",
+    "url": "/cropper/static/js/runtime-main.a456c963.js"
+  }
+]);
